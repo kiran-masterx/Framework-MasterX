e8b9fa443a46fbfa9c0ef32c9161e38330af26e2
kiran <kiran.masterx@gmail.com>
2023-01-16 20:02:43 +0530

    Added one more test case in FirstTestCase.java

--- src/test/java/testCases/FirstTestCase.java
+++ src/test/java/testCases/FirstTestCase.java
@@ -11,14 +11,14 @@ import pageObjects.HomePage;
 import pageObjects.LoginPage;
 
 public class FirstTestCase extends BasePage {
-	
+
 	@BeforeMethod
 	public void setUp() {
 		launchBrowser();
 		LoginPage loginPage = new LoginPage();
 		loginPage.cogmentoLogin();
 	}
-	
+
 	@Test
 	public void verifyLogoDisplayed() throws IOException {
 		test = extent.createTest("verifyLogoDisplayed", "This is to verify the logo is displaying or not");
@@ -26,28 +26,30 @@ public class FirstTestCase extends BasePage {
 		homePage.verifyLogo();
 		captureScreenshots("verifyLogoDisplayed");
 	}
-	
+
 	@Test
-	public void verifyUserIsTestUser() throws IOException {		
+	public void verifyUserIsTestUser() throws IOException {
 		test = extent.createTest("verifyUserIsTestUser", "This is top verify correct user is displying or not");
 		HomePage homePage = new HomePage();
 		homePage.verifyUser();
 		captureScreenshots("verifyUserIsTestUser");
 	}
-	
+
+	@Test
+	public void verifySum() {
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		assert a + b == c;
+	}
+
 	@AfterMethod
 	public void tearDown() {
 		driver.quit();
 		extent.flush();
 	}
-	
-	
-	
-	
-	
+
 }
-
-
 
 //Test cases
 //1. Verify the cogmento logo is displayed
